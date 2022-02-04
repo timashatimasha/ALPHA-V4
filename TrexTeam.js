1bc82ecf26021da02873595d49c8721c8eee0b97
timashatimasha <98791752+timashatimasha@users.noreply.github.com>
2022-02-04 03:47:05 -0800

    Update TrexTeam.js

--- TrexTeam.js
+++ TrexTeam.js
@@ -1,5 +1,5 @@
 module.exports = {
-    ALPHA_OWNER: '94784506970', // Trex Team - Alpha Owner
-    TREX_TEAM: '94771039631,94784506970,94716184907,94711421243,94771251788', // Team - Trex TEAM
-    TREX_OWNER: '94771039631' // Hirush Rukshan - Trex Owner --> For The Friendship And co Owner in Alpha 
+    ALEXA_OWNER: '94773585511', // - Alexa Owner
+    ALEXA_TEAM: ',94715264791,94771920675', // Team - alexa 
+    ALEXA_co owner: '94771039631' // W.L Lushen - Queen Alexa Team --> For The Friendship And co Owner in Alexa 
 };
